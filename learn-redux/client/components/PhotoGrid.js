65950e5a624cf85ecdddd86ba5411c1ade51fc38
Thiago Durante Pires <thdurante@gmail.com>
2017-03-27 20:10:54 -0300

    App Layout + Component setup Video #02

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PhotoGrid extends React.Component {
+
+    render() {
+        return (
+            <div>PhotoGrid</div>
+        )
+    }
+}
+
+export default PhotoGrid;
