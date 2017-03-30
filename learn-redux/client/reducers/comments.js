affdaaa122d862d579e0a17aa0fa1580e5dc91fc
Thiago Durante Pires <thdurante@gmail.com>
2017-03-30 10:30:35 -0300

    Integrating the store with React Router Video #08

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -11,4 +11,4 @@ function comments(state = [], action) {
     return state;
 }
 
-export default posts;
+export default comments;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import css from './styles/style.styl';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
 const router = (
-    <Router history={browserHistory}>
-        <Route path="/" component={Main}>
-            <IndexRoute component={PhotoGrid}></IndexRoute>
-            <Route path="/view/:postId" component={Single}></Route>
-        </Route>
-    </Router>
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={Main}>
+                <IndexRoute component={PhotoGrid}></IndexRoute>
+                <Route path="/view/:postId" component={Single}></Route>
+            </Route>
+        </Router>
+    </Provider>
 )
 
 render(router, document.querySelector('#root'));
