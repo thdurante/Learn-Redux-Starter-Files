f9182fcf9d4b539e783fc8a1971c9ec25f8b2097
Thiago Durante Pires <thdurante@gmail.com>
2017-03-30 12:23:57 -0300

    Displaying Redux state inside the components Video #11

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Photo from './Photo';
 
 class PhotoGrid extends React.Component {
 
     render() {
         return (
             <div className="photo-grid ">
-                PhotoGrid
+                {this.props.posts.map((post, index) => <Photo {...this.props} key={index} index={index} post={post} />)}
             </div>
         )
     }
