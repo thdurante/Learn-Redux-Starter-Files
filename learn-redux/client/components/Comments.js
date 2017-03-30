f9b6bc7237dbbf8ceab0e124e1a8911aa73ca0b0
Thiago Durante Pires <thdurante@gmail.com>
2017-03-30 16:03:09 -0300

    Displaying the single photo component Video #13

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Comments extends React.Component {
+    render() {
+        return (
+            <div className="comment"></div>
+        );
+    }
+}
+
+export default Comments;
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 class Single extends React.Component {
-
     render() {
+        const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId)
+        const post = this.props.posts[index];
+
         return (
             <div className="single-photo">
-                Single
+                <Photo index={index} post={post} {...this.props} />
+                <Comments/>
             </div>
         )
     }
